8394367f477014daef2308c949af372670772c94
th31dk <andrew.kxu@gmail.com>
2025-12-24 13:58:33 -0800

    website fix

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000); // Black background
+scene.background = new THREE.Color(0x000000);
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -16,13 +16,13 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio( window.devicePixelRatio );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const texture = new THREE.TextureLoader().load('./jon.png')
+const texture = new THREE.TextureLoader().load('/jon.png');
 const material = new THREE.MeshBasicMaterial({map: texture});
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
-const donut_tex = new THREE.TextureLoader().load('./jon.png')
+const donut_tex = new THREE.TextureLoader().load('/jon.png');
 const donut_mat = new THREE.MeshBasicMaterial({map: donut_tex});
 const donut = new THREE.Mesh(donut_geo, donut_mat);
 scene.add(donut);
